66f557d888153cca452d801cc32f3f4b13e99415
Axel Huebl <axel.huebl@plasma.ninja>
2021-02-12 01:18:09 -0800

    Add CI: Linux/macOS/Windows

---
+++ .github/workflows/dependencies/dependencies_mac.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 The AMReX Community
+#
+# License: BSD-3-Clause-LBNL
+# Authors: Axel Huebl
+
+set -eu -o pipefail
+
+brew update
+brew install gfortran || true
+brew install libomp || true
+brew install open-mpi || true
