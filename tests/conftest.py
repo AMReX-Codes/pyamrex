236820d405d29c27433020cf9b925cc7efc47626
Shreyas Ananthan <shreyas@umd.edu>
2021-02-13 20:57:28 -0700

    Attempt to fix MPI init issues with pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,9 @@
 import pytest
 import pyamrex as amrex
 
+if amrex.have_mpi():
+    from mpi4py import MPI
+
 @pytest.fixture(autouse=True, scope='session')
 def amrex_init():
     amrex.initialize(["amrex.verbose=-1"])
