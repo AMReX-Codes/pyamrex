f6bd4b3e2a3c96e60c15bffe3c6d2b4c5cab3195
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-14 20:58:55 +0700

    Bump Version: 22.06

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ with open('./requirements.txt') as f:
 setup(
     name='amrex',
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version = '21.04',
+    version = '22.06',
     packages = ['amrex'],
 
     # Python sources:
