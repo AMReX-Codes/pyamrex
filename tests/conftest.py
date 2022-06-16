0f404859a62f252bf2513d3f20d566e24ed99a54
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-16 13:41:54 +0700

    Tests: Increase AMReX Verbosity (#48)
    Keep AMReX status output on for tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,17 @@ if amrex.Config.have_mpi:
 
 @pytest.fixture(autouse=True, scope='session')
 def amrex_init():
-    amrex.initialize(["amrex.verbose=-1"])
+    amrex.initialize([
+        # print AMReX status messages
+        "amrex.verbose=2",
+        # throw exceptions and create core dumps instead of
+        # AMReX backtrace files: allows to attach to
+        # debuggers
+        "amrex.throw_exception=1",
+        "amrex.signal_handling=0",
+        # abort GPU runs if out-of-memory instead of swapping to host RAM
+        #"abort_on_out_of_gpu_memory=1",
+    ])
     yield
     amrex.finalize()
 
