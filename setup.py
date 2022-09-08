e66ebdef0e76f0cb63c3321280d46f3f50647eb0
Axel Huebl <axel.huebl@plasma.ninja>
2022-09-08 11:38:46 -0700

    Bump Version: 22.09

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ with open("./requirements.txt") as f:
 setup(
     name="amrex",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="22.06",
+    version="22.09",
     packages=["amrex"],
     # Python sources:
     package_dir={"": "src"},
