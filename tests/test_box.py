5baa64f33846a14a1f6546dd62987ed5cd8f5773
Axel Huebl <axel.huebl@plasma.ninja>
2022-09-09 02:16:05 -0700

    Box::makeSlab
    Adds a binding to `Box::makeSlab`.

--- tests/test_box.py
+++ tests/test_box.py
@@ -39,6 +39,14 @@ def test_grow(box):
     assert bx.num_pts == (134**3)
 
 
+def test_slab(box):
+    """box.make_slab"""
+    box.make_slab(direction=3, slab_index=60)
+    np.testing.assert_allclose(box.lo_vect, [0, 0, 0])
+    np.testing.assert_allclose(box.hi_vect, [60, 127, 127])
+    assert box.num_pts == (128 * 128 * 61)
+
+
 # def test_convert(box):
 #    """Conversion to node"""
 #    bx = box.convert(amrex.CellIndex.NODE, amrex.CellIndex.NODE, amrex.CellIndex.NODE)
