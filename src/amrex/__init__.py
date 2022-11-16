ca6ceaaebca5dc4fb92ec1a24644966be469821a
Axel Huebl <axel.huebl@plasma.ninja>
2022-11-16 11:15:32 -0600

    Windows: Add PATH to .dll directories
    Python 3.8+ on Windows: DLL search paths for dependent
    shared libraries.
    
    Refs.:
    - https://github.com/python/cpython/issues/80266
    - https://docs.python.org/3.8/library/os.html#os.add_dll_directory

--- src/amrex/__init__.py
+++ src/amrex/__init__.py
@@ -1,3 +1,24 @@
+import os
+
+# Python 3.8+ on Windows: DLL search paths for dependent
+# shared libraries
+# Refs.:
+# - https://github.com/python/cpython/issues/80266
+# - https://docs.python.org/3.8/library/os.html#os.add_dll_directory
+if os.name == "nt":
+    # add anything in the current directory
+    pwd = __file__.rsplit(os.sep, 1)[0] + os.sep
+    os.add_dll_directory(pwd)
+    # add anything in PATH
+    paths = os.environ.get("PATH", "")
+    for p in paths.split(";"):
+        print(f"p={p}")
+        if os.path.exists(p):
+            os.add_dll_directory(p)
+        else:
+            print("  ... does NOT exist")
+
+# import core bindings to C++
 from . import amrex_pybind
 from .amrex_pybind import *  # noqa
 
