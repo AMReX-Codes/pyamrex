cffda2cce20a59e631bf9959d9c4bedb63a316a4
Axel Huebl <axel.huebl@plasma.ninja>
2022-12-06 16:37:50 -0800

    pyAMReX: 22.12

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ with open("./requirements.txt") as f:
 setup(
     name="amrex",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="22.11",
+    version="22.12",
     packages=["amrex"],
     # Python sources:
     package_dir={"": "src"},
