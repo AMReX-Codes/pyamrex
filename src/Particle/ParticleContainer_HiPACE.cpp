a3b52ecbb317ebd4716c2d493c3fa0c50225edb5
Axel Huebl <axel.huebl@plasma.ninja>
2023-02-10 17:59:12 -0800

    Unused Param

--- src/Particle/ParticleContainer_HiPACE.cpp
+++ src/Particle/ParticleContainer_HiPACE.cpp
@@ -6,7 +6,7 @@
 #include "ParticleContainer.H"
 
 
-void init_ParticleContainer_HiPACE(py::module& m) {
+void init_ParticleContainer_HiPACE(py::module& /* m */) {
     // TODO: we might need to move all or most of the defines in here into a
     //       test/example submodule, so they do not collide with downstream projects
     //make_ParticleContainer_and_Iterators< 0, 0, 4, 0> (m);   // HiPACE++ 22.07
