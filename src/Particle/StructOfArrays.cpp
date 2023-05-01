95df181439d7b198c37ad7d76b16018cb71b07c3
Axel Huebl <axel.huebl@plasma.ninja>
2023-05-01 09:09:25 -0700

    Release: 23.05
    The May release :)

--- src/Particle/StructOfArrays.cpp
+++ src/Particle/StructOfArrays.cpp
@@ -72,7 +72,7 @@ void make_StructOfArrays(py::module &m)
 void init_StructOfArrays(py::module& m) {
     make_StructOfArrays< 2, 1>(m);
     make_StructOfArrays< 4, 0>(m);  // HiPACE++ 22.07
-    make_StructOfArrays< 5, 0>(m);  // ImpactX 22.07 - 23.04
-    make_StructOfArrays< 8, 2>(m);  // ImpactX 23.04+
+    make_StructOfArrays< 5, 0>(m);  // ImpactX 22.07 - 23.06
+    make_StructOfArrays< 8, 2>(m);  // ImpactX 23.06+
     make_StructOfArrays<37, 1>(m);  // HiPACE++ 22.07
 }
