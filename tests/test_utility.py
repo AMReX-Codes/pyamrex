06006bfde1e42abb71975ca9630345a16eceebef
Axel Huebl <axel.huebl@plasma.ninja>
2023-07-18 17:51:19 -0700

    Cleaning

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -6,6 +6,4 @@ import amrex.space3d as amr
 
 def test_concatenate():
     pltname = amr.concatenate("plt", 1000, 5)
-    print("--test concatenate --")
-    print("plotfile name", pltname)
     assert pltname == "plt01000"
