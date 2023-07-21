1b31a0e5afc4168dd44ec0334b6add99e51d53a6
Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
2023-07-21 14:16:44 -0700

    initialize-finalize called once for all tests
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- tests/test_parmparse.py
+++ tests/test_parmparse.py
@@ -7,15 +7,13 @@ def test_parmparse():
     dopml = False
     ncell = 10
     dt = 0.0
-    # Since ParmParse is done in IO Processors, we need amr.initialize first
-    amr.initialize([])
     pp = amr.ParmParse("")
     pp.addfile("./parmparse_inputs")
     pp_param = amr.ParmParse("param")
     (_, ncell) = pp_param.query_int("ncell")
     dt = pp_param.get_real("dt")
     dopml = pp_param.get_bool("do_pml")
+
     assert dopml == True
     assert dt == 1.0e-5
     assert ncell == 100
-    amr.finalize()
