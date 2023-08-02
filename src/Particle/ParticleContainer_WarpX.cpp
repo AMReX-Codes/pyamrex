36895836fab253731db8d80ca4230ecb7d126eb2
Axel Huebl <axel.huebl@plasma.ninja>
2023-08-02 08:38:14 -0700

    Release: 23.08 (#166)
    The August release :)

--- src/Particle/ParticleContainer_WarpX.cpp
+++ src/Particle/ParticleContainer_WarpX.cpp
@@ -13,6 +13,6 @@ void init_ParticleContainer_WarpX(py::module& m) {
 
     // TODO: we might need to move all or most of the defines in here into a
     //       test/example submodule, so they do not collide with downstream projects
-    make_ParticleContainer_and_Iterators<Particle<0, 0>, 4, 0>(m);   // WarpX 22.07 - 23.07 1D-3D
-    //make_ParticleContainer_and_Iterators<Particle<0, 0>, 5, 0> (m);   // WarpX 22.07 - 23.07 RZ
+    make_ParticleContainer_and_Iterators<Particle<0, 0>, 4, 0>(m);   // WarpX 22.07 - 23.08 1D-3D
+    //make_ParticleContainer_and_Iterators<Particle<0, 0>, 5, 0> (m);   // WarpX 22.07 - 23.08 RZ
 }
