cf07bfdd352a73376c95fdf798cecdb63deb7b6b
ax3l <ax3l@users.noreply.github.com>
2023-10-04 19:13:15 +0000

    Update Stub Files

--- src/amrex/space1d/__init__.pyi
+++ src/amrex/space1d/__init__.pyi
@@ -492,4 +492,4 @@ def d_decl(x, y, z):
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "23.09"
+__version__: str = "23.10"
--- src/amrex/space3d/amrex_3d_pybind/__init__.pyi
+++ src/amrex/space3d/amrex_3d_pybind/__init__.pyi
@@ -3567,7 +3567,7 @@ class BoxArray:
     def size(self) -> int: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "23.09"
+    amrex_version: typing.ClassVar[str] = "23.10"
     gpu_backend = None
     have_gpu: typing.ClassVar[bool] = False
     have_mpi: typing.ClassVar[bool] = True
@@ -10886,4 +10886,4 @@ def write_single_level_plotfile(
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "23.09"
+__version__: str = "23.10"
