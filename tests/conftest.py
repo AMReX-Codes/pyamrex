6c264e738fd09a95568766190de0b0b83f09457a
Axel Huebl <axel.huebl@plasma.ninja>
2023-10-19 14:17:04 -0700

    Tests: Managed Memory
    Recently, AMReX' default changed away from defaulting the `Arena` to
    managed memory. Some of our tests still rely on it, so we keep the
    old default for now.

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,6 +37,8 @@ def amrex_init(tmpdir):
                 "amrex.signal_handling=0",
                 # abort GPU runs if out-of-memory instead of swapping to host RAM
                 # "abort_on_out_of_gpu_memory=1",
+                # the arena for GPU runs defaults to managed
+                "amrex.the_arena_is_managed=1",
             ]
         )
         yield
