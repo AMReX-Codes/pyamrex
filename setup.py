757207b28bfabb96b6c8f5b4d753a79124b7b565
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-04 08:53:15 -0800

    pyAMReX: 23.12

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ with open("./requirements.txt") as f:
 setup(
     name="amrex",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="23.11",
+    version="23.12",
     packages=["amrex"],
     # Python sources:
     package_dir={"": "src"},
--- src/Particle/ParticleContainer_WarpX.cpp
+++ src/Particle/ParticleContainer_WarpX.cpp
@@ -13,6 +13,6 @@ void init_ParticleContainer_WarpX(py::module& m) {
 
     // TODO: we might need to move all or most of the defines in here into a
     //       test/example submodule, so they do not collide with downstream projects
-    make_ParticleContainer_and_Iterators<Particle<0, 0>, 4, 0>(m);   // WarpX 22.07 - 23.11 1D-3D
-    //make_ParticleContainer_and_Iterators<Particle<0, 0>, 5, 0> (m);   // WarpX 22.07 - 23.11 RZ
+    make_ParticleContainer_and_Iterators<Particle<0, 0>, 4, 0>(m);   // WarpX 22.07 - 23.12 1D-3D
+    //make_ParticleContainer_and_Iterators<Particle<0, 0>, 5, 0> (m);   // WarpX 22.07 - 23.12 RZ
 }
