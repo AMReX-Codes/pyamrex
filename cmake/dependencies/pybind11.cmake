0b0ee42404f32ec3f62c3322dedbd27699ad9668
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-04 12:01:56 -0800

    Release: 23.12 (#228)
    * pybind11: 2.11.1+
    
    * AMReX: 23.12
    
    * pyAMReX: 23.12

--- cmake/dependencies/pybind11.cmake
+++ cmake/dependencies/pybind11.cmake
@@ -41,7 +41,7 @@ function(find_pybind11)
             mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_FETCHEDpybind11)
         endif()
     elseif(NOT pyAMReX_pybind11_internal)
-        find_package(pybind11 2.10.1 CONFIG REQUIRED)
+        find_package(pybind11 2.11.1 CONFIG REQUIRED)
         message(STATUS "pybind11: Found version '${pybind11_VERSION}'")
     endif()
 endfunction()
@@ -56,7 +56,7 @@ option(pyAMReX_pybind11_internal "Download & build pybind11" ON)
 set(pyAMReX_pybind11_repo "https://github.com/pybind/pybind11.git"
     CACHE STRING
     "Repository URI to pull and build pybind11 from if(pyAMReX_pybind11_internal)")
-set(pyAMReX_pybind11_branch "v2.10.1"
+set(pyAMReX_pybind11_branch "v2.11.1"
     CACHE STRING
     "Repository branch for pyAMReX_pybind11_repo if(pyAMReX_pybind11_internal)")
 
--- src/Particle/StructOfArrays.cpp
+++ src/Particle/StructOfArrays.cpp
@@ -89,8 +89,8 @@ void make_StructOfArrays(py::module &m)
 
 void init_StructOfArrays(py::module& m) {
     make_StructOfArrays< 2, 1>(m);
-    make_StructOfArrays< 4, 0>(m);  // HiPACE++ 22.08 - 23.11
-    make_StructOfArrays< 5, 0>(m);  // ImpactX 22.07 - 23.11
-    make_StructOfArrays< 8, 2>(m);  // ImpactX 23.12+
-    make_StructOfArrays<37, 1>(m);  // HiPACE++ 22.09 - 23.11
+    make_StructOfArrays< 4, 0>(m);  // HiPACE++ 22.08 - 23.12
+    make_StructOfArrays< 5, 0>(m);  // ImpactX 22.07 - 23.12
+    make_StructOfArrays< 8, 2>(m);  // ImpactX 24.01+
+    make_StructOfArrays<37, 1>(m);  // HiPACE++ 22.09 - 23.12
 }
