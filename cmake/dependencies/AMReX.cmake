d9787b43d1fd0a8831d88f1bb559cc9f512b92e4
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-11 21:46:59 -0800

    AMReX: Update to latest development (#232)
    * AMReX: Update to latest development
    
    Update to latest commit in `development`.
    
    Introduces the new `idcpu` treatment in pure SoA particles:
    https://github.com/AMReX-Codes/amrex/pull/3585
    
    * ImpactX 2024: Pure SoA Update
    
    * AMReX: Update to latest development

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -85,7 +85,7 @@ option(pyAMReX_amrex_internal "Download & build AMReX" ON)
 set(pyAMReX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(pyAMReX_amrex_internal)")
-set(pyAMReX_amrex_branch "23.12"
+set(pyAMReX_amrex_branch "ecaa46d0be4b5c79b8806e48e3469000d8bb7252"
     CACHE STRING
     "Repository branch for pyAMReX_amrex_repo if(pyAMReX_amrex_internal)")
 
