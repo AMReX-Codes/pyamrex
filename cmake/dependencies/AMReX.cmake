dadcb94a16e79430c2e784a0bbea16edd424e00c
Axel Huebl <axel.huebl@plasma.ninja>
2024-03-29 16:19:52 -0700

    Update AMreX

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -89,7 +89,7 @@ option(pyAMReX_amrex_internal "Download & build AMReX" ON)
 set(pyAMReX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(pyAMReX_amrex_internal)")
-set(pyAMReX_amrex_branch "fe6f3de74333ccf7e4babb4832602cf6c108ce52"
+set(pyAMReX_amrex_branch "47347f785f5c26f1f9e65bef8e10f2d383406ef7"
     CACHE STRING
     "Repository branch for pyAMReX_amrex_repo if(pyAMReX_amrex_internal)")
 
