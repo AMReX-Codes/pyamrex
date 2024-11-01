4c4ee46e5b0cfa6ed5040bef2d0a0ab699ea78be
David Grote <grote1@llnl.gov>
2024-11-01 10:37:16 -0700

    Add version of Read that takes MultiFab as argument

--- src/Base/VisMF.cpp
+++ src/Base/VisMF.cpp
@@ -32,5 +32,15 @@ void init_VisMF(py::module &m)
            py::return_value_policy::move,
            py::arg("name"),
            "Reads a MultiFab from the specified file")
+        .def_static("Read",
+           [](const std::string &name, amrex::MultiFab &mf) {
+               if (amrex::VisMF::Exist(name)) {
+                   amrex::VisMF::Read(mf, name);
+               } else {
+                   throw std::runtime_error("MultiFab file " + name + " couldn't be found!");
+               }
+               },
+           py::arg("name"), py::arg("mf"),
+           "Reads a MultiFab from the specified file into the given MultiFab. The BoxArray on the disk must match the BoxArray * in mf")
         ;
 }
