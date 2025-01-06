44c82f0198d4c56cbcb22f905dc53f94c6083608
Axel Huebl <axel.huebl@plasma.ninja>
2025-01-05 18:07:19 -0800

    Refactor

--- src/Base/SmallMatrix.cpp
+++ src/Base/SmallMatrix.cpp
@@ -16,20 +16,9 @@ void init_SmallMatrix (py::module &m)
     {
         constexpr int NRows = 6;
         constexpr int NCols = 6;
-        constexpr Order ORDER = Order::F;
+        constexpr amrex::Order ORDER = amrex::Order::F;
         constexpr int StartIndex = 1;
 
-        // Vector
-        make_SmallMatrix< float, NRows, 1, ORDER, StartIndex >(m, "float");
-        make_SmallMatrix< double, NRows, 1, ORDER, StartIndex >(m, "double");
-        make_SmallMatrix< long double, NRows, 1, ORDER, StartIndex >(m, "longdouble");
-
-        // RowVector
-        make_SmallMatrix< float, 1, NCols, ORDER, StartIndex >(m, "float");
-        make_SmallMatrix< double, 1, NCols, ORDER, StartIndex >(m, "double");
-        make_SmallMatrix< long double, 1, NCols, ORDER, StartIndex >(m, "longdouble");
-
-        // Matrix
         make_SmallMatrix< float, NRows, NCols, ORDER, StartIndex >(m, "float");
         make_SmallMatrix< double, NRows, NCols, ORDER, StartIndex >(m, "double");
         make_SmallMatrix< long double, NRows, NCols, ORDER, StartIndex >(m, "longdouble");
