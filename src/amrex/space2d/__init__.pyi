7697686ef2f40adf26e903db7ebac65898ad31e8
ax3l <1353258+ax3l@users.noreply.github.com>
2025-01-23 05:15:30 +0000

    Update Stub Files

--- src/amrex/space2d/__init__.pyi
+++ src/amrex/space2d/__init__.pyi
@@ -627,4 +627,4 @@ def d_decl(x, y, z):
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.01"
+__version__: str = "25.01-25-g0f46a1615c17"
--- src/amrex/space2d/amrex_2d_pybind/__init__.pyi
+++ src/amrex/space2d/amrex_2d_pybind/__init__.pyi
@@ -5078,7 +5078,7 @@ class BoxArray:
     def size(self) -> int: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.01"
+    amrex_version: typing.ClassVar[str] = "25.01-25-g0f46a1615c17"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = True
     have_gpu: typing.ClassVar[bool] = False
@@ -22026,5 +22026,5 @@ def write_single_level_plotfile(
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.01"
+__version__: str = "25.01-25-g0f46a1615c17"
 IntVect = IntVect2D
