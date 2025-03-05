f8746e74d226c534ce306805639816ad25f3199a
ax3l <1353258+ax3l@users.noreply.github.com>
2025-03-05 20:56:03 +0000

    Update Stub Files

--- src/amrex/space3d/amrex_3d_pybind/__init__.pyi
+++ src/amrex/space3d/amrex_3d_pybind/__init__.pyi
@@ -363,7 +363,7 @@ class Arena:
     @staticmethod
     def finalize() -> None: ...
     @staticmethod
-    def initialize() -> None: ...
+    def initialize(arg0: bool) -> None: ...
     @staticmethod
     def print_usage() -> None: ...
     @staticmethod
@@ -4985,7 +4985,7 @@ class BoxArray:
     def size(self) -> int: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.02"
+    amrex_version: typing.ClassVar[str] = "25.03"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = True
     have_gpu: typing.ClassVar[bool] = False
@@ -20726,5 +20726,5 @@ def write_single_level_plotfile(
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.02"
+__version__: str = "25.03"
 IntVect = IntVect3D
