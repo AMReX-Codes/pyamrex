500cf49d2f2addb7893c0750f225c90b0234ba2b
WeiqunZhang <7862753+WeiqunZhang@users.noreply.github.com>
2025-04-04 17:24:45 +0000

    Update Stub Files

--- src/amrex/space2d/amrex_2d_pybind/__init__.pyi
+++ src/amrex/space2d/amrex_2d_pybind/__init__.pyi
@@ -5009,7 +5009,7 @@ class BoxArray:
     def size(self) -> int: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.03"
+    amrex_version: typing.ClassVar[str] = "25.04"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = True
     have_gpu: typing.ClassVar[bool] = False
@@ -22481,5 +22481,5 @@ def write_single_level_plotfile(
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.03"
+__version__: str = "25.04"
 IntVect = IntVect2D
