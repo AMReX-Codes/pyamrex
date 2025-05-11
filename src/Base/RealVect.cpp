347a4112d56a551c2d13ef5e905f910fd5d19cff
Weiqun Zhang <weiqunzhang@lbl.gov>
2025-05-11 10:09:25 -0700

    Try to fix BASISREALV

--- src/Base/RealVect.cpp
+++ src/Base/RealVect.cpp
@@ -125,7 +125,9 @@ void init_RealVect(py::module &m) {
           .def_static("zero_vector", &RealVect::TheZeroVector)
           .def_static("unit_vector", &RealVect::TheUnitVector)
 
-          .def("BASISREALV", &BASISREALV<AMREX_SPACEDIM>, "return basis vector in given coordinate direction")
+          .def("BASISREALV", [](int dir) {
+              return BASISREALV<AMREX_SPACEDIM>(dir); },
+              "return basis vector in given coordinate direction")
      ;
 
      m.def("min", [](const RealVect& a, const RealVect& b) {
