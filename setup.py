58c6342befe9914739118911f91ce2cac9688e2c
Edoardo Zoni <ezoni@lbl.gov>
2025-09-03 09:54:04 -0700

    Release: pyAMReX 25.09

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ with open("./requirements.txt") as f:
 setup(
     name="amrex",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="25.08",
+    version="25.09",
     packages=["amrex"],
     # Python sources:
     package_dir={"": "src"},
