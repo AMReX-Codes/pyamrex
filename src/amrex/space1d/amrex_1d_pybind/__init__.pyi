c4740ef4a5ef7f85ece6e79db104d948f9402523
EZoni <59625522+EZoni@users.noreply.github.com>
2025-10-01 21:03:49 +0000

    Update Stub Files

--- src/amrex/space1d/amrex_1d_pybind/__init__.pyi
+++ src/amrex/space1d/amrex_1d_pybind/__init__.pyi
@@ -5472,7 +5472,7 @@ class BoxArray:
     def size(self) -> int: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.09-4-g95822df1a393"
+    amrex_version: typing.ClassVar[str] = "25.10"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = False
     have_gpu: typing.ClassVar[bool] = False
@@ -22646,5 +22646,5 @@ def write_single_level_plotfile(
 
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.09-4-g95822df1a393"
+__version__: str = "25.10"
 IntVect = IntVect1D
