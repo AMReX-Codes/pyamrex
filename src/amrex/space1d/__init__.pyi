6d48d1c792c134d1bb9e4d83e0a78b74274ff218
ax3l <1353258+ax3l@users.noreply.github.com>
2025-12-04 02:10:06 +0000

    Update Stub Files

--- src/amrex/space1d/__init__.pyi
+++ src/amrex/space1d/__init__.pyi
@@ -608,4 +608,4 @@ Geometric: amrex_1d_pybind.GrowthStrategy  # value = <GrowthStrategy.Geometric: 
 Poisson: amrex_1d_pybind.GrowthStrategy  # value = <GrowthStrategy.Poisson: 0>
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.11-15-g4dad1664d746"
+__version__: str = "25.12"
--- src/amrex/space2d/amrex_2d_pybind/__init__.pyi
+++ src/amrex/space2d/amrex_2d_pybind/__init__.pyi
@@ -358,7 +358,7 @@ class AMReX:
     def top() -> AMReX: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.11-15-g4dad1664d746"
+    amrex_version: typing.ClassVar[str] = "25.12"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = True
     have_gpu: typing.ClassVar[bool] = False
@@ -26609,7 +26609,7 @@ Geometric: GrowthStrategy  # value = <GrowthStrategy.Geometric: 2>
 Poisson: GrowthStrategy  # value = <GrowthStrategy.Poisson: 0>
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.11-15-g4dad1664d746"
+__version__: str = "25.12"
 basic: EBSupport  # value = <EBSupport.basic: 1>
 full: EBSupport  # value = <EBSupport.full: 3>
 volume: EBSupport  # value = <EBSupport.volume: 2>
