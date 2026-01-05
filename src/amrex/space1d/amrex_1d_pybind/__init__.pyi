91d9ee0fdd7d70fc7b80c8392cb3b19524280571
ax3l <1353258+ax3l@users.noreply.github.com>
2026-01-05 22:34:12 +0000

    Update Stub Files

--- src/amrex/space1d/amrex_1d_pybind/__init__.pyi
+++ src/amrex/space1d/amrex_1d_pybind/__init__.pyi
@@ -327,7 +327,7 @@ class AMReX:
     def top() -> AMReX: ...
 
 class Config:
-    amrex_version: typing.ClassVar[str] = "25.12"
+    amrex_version: typing.ClassVar[str] = "26.01"
     gpu_backend = None
     have_eb: typing.ClassVar[bool] = False
     have_gpu: typing.ClassVar[bool] = False
@@ -24255,5 +24255,5 @@ Geometric: GrowthStrategy  # value = <GrowthStrategy.Geometric: 2>
 Poisson: GrowthStrategy  # value = <GrowthStrategy.Poisson: 0>
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.12"
+__version__: str = "26.01"
 IntVect = IntVect1D
