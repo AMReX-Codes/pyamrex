6d21312e16fd2d9951d07723bb755403c0fea7ae
Axel Huebl <axel.huebl@plasma.ninja>
2026-02-23 23:06:23 -0800

    SmallMatrix: 3x6 & 3x1
    Matrix & vector type commonly used in accelerator physics w/ spin.

--- src/Base/SmallMatrix.cpp
+++ src/Base/SmallMatrix.cpp
@@ -13,8 +13,27 @@ void init_SmallMatrix (py::module &m)
     using namespace pyAMReX;
 
     // 6x6 Matrix as commonly used in accelerator physics
+    //   used by ImpactX
     {
         constexpr int NRows = 6;
+        constexpr int NCols = 6;
+        constexpr amrex::Order ORDER = amrex::Order::F;
+        constexpr int StartIndex = 1;
+
+        make_SmallMatrix< float, NRows, NCols, ORDER, StartIndex >(m, "float");
+        make_SmallMatrix< double, NRows, NCols, ORDER, StartIndex >(m, "double");
+        make_SmallMatrix< long double, NRows, NCols, ORDER, StartIndex >(m, "longdouble");
+        /*
+        make_SmallMatrix< float const, NRows, NCols, ORDER, StartIndex >(m, "float_const");
+        make_SmallMatrix< double const, NRows, NCols, ORDER, StartIndex >(m, "double_const");
+        make_SmallMatrix< long double const, NRows, NCols, ORDER, StartIndex >(m, "longdouble_const");
+        */
+    }
+
+    // 3x6 Matrix as commonly used in accelerator physics w/ Spin
+    //   used by ImpactX
+    {
+        constexpr int NRows = 3;
         constexpr int NCols = 6;
         constexpr amrex::Order ORDER = amrex::Order::F;
         constexpr int StartIndex = 1;
