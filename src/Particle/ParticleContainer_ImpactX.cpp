b570bacf8a441424aa18a33aa37ff12b73fe60d1
Axel Huebl <axel.huebl@plasma.ninja>
2026-06-09 08:46:44 -0700

    Particles: Templated Real Type
    If we want to develop applications that can use double and single
    precision switches at runtime, then we need to be able to generate
    both in the same binary. This adds the corresponding templates
    (that default to the current status quo) in particle types.

--- src/Particle/ParticleContainer_ImpactX.cpp
+++ src/Particle/ParticleContainer_ImpactX.cpp
@@ -16,5 +16,11 @@ void init_ParticleContainer_ImpactX(py::module& m) {
 
     // TODO: we might need to move all or most of the defines in here into a
     //       test/example submodule, so they do not collide with downstream projects
-    make_ParticleContainer_and_Iterators<SoAParticle<11, 0>, 11, 0, only_polymorphic>(m);  // ImpactX 26.01+
+    make_ParticleContainer_and_Iterators<SoAParticle<11, 0, ParticleReal>, 11, 0, only_polymorphic>(m);  // ImpactX 26.01+ (native ParticleReal precision)
+#ifndef AMREX_SINGLE_PRECISION_PARTICLES
+    // additionally provide a single-precision storage container for runtime
+    // precision selection (named with a "_sp" suffix); when particles are
+    // already single precision the native registration above already covers it
+    make_ParticleContainer_and_Iterators<SoAParticle<11, 0, float>, 11, 0, only_polymorphic>(m);  // ImpactX single precision
+#endif
 }
