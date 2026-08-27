adbbaf6c81751e81b02eaa672eb36fb9836146f8
ax3l <1353258+ax3l@users.noreply.github.com>
2026-08-27 20:10:20 +0000

    Update Stub Files

--- src/amrex/space1d/__init__.pyi
+++ src/amrex/space1d/__init__.pyi
@@ -771,7 +771,7 @@ Geometric: amrex_1d_pybind.GrowthStrategy
 Poisson: amrex_1d_pybind.GrowthStrategy
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "26.08-11-g5273b558c130"
+__version__: str = "26.08-31-g06424932b757"
 kDLCPU: amrex_1d_pybind.DLDeviceType
 kDLCUDA: amrex_1d_pybind.DLDeviceType
 kDLCUDAHost: amrex_1d_pybind.DLDeviceType
--- src/amrex/space3d/__init__.pyi
+++ src/amrex/space3d/__init__.pyi
@@ -782,7 +782,7 @@ Geometric: amrex_3d_pybind.GrowthStrategy
 Poisson: amrex_3d_pybind.GrowthStrategy
 __author__: str = "Axel Huebl, Ryan T. Sandberg, Shreyas Ananthan, David P. Grote, Revathi Jambunathan, Edoardo Zoni, Remi Lehe, Andrew Myers, Weiqun Zhang"
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "26.08-11-g5273b558c130"
+__version__: str = "26.08-31-g06424932b757"
 basic: amrex_3d_pybind.EBSupport
 full: amrex_3d_pybind.EBSupport
 kDLCPU: amrex_3d_pybind.DLDeviceType
